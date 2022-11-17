62d55437f4bc482deca234f2f9201e42e3b0f6f5
KuchenMann <65622916+Hunter599@users.noreply.github.com>
2022-11-17 10:50:50 -0300

    17/11

--- JogoTecnicasProgramacao/Fase_Primeira.cpp
+++ JogoTecnicasProgramacao/Fase_Primeira.cpp
@@ -20,11 +20,14 @@ namespace Fases {
 	void Fases::Fase_Primeira::executar()
 	{
 		//cout << "fase1 exe" << endl;
+		
 		LEnt.executarTodos();
+		this->imprimir();
 	}
 
 	void Fase_Primeira::imprimir()
 	{
+		this->pGerenteGrafico->render(&corpo);
 	}
 
 
